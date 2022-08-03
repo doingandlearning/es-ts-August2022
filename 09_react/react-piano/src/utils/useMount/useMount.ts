59530245fd766bba105379b2b9d71230159ed6ed
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-08-03 11:15:10 +0100

    Tidy up the directory.

--- 09_react/react-piano/src/utils/useMount/useMount.ts
+++ 09_react/react-piano/src/utils/useMount/useMount.ts
@@ -1,14 +1,14 @@
-import { EffectCallback, useEffect } from "react"
+import { EffectCallback, useEffect } from "react";
 
 const useEffectOnce = (effect: EffectCallback) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect, [])
-}
+  useEffect(effect, []);
+};
 
-type Effect = (...args: unknown[]) => void
+type Effect = (...args: unknown[]) => void;
 
 export const useMount = (fn: Effect) => {
   useEffectOnce(() => {
-    fn()
-  })
-}
+    fn();
+  });
+};
